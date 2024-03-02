fe726b0d2f97b73dacc8a492c7679a4109dda32e
Jyoti Ranjan Das <jyotiranjandas@Jyotis-MacBook-Air.local>
2024-03-02 22:53:10 +0530

    adding push notif

--- index.js
+++ index.js
@@ -2,19 +2,33 @@ import express from "express";
 import dotenv from "dotenv";
 import { google } from "googleapis";
 import { OAuth2Client } from "google-auth-library";
+import { PubSub } from "@google-cloud/pubsub";
 
 dotenv.config();
 
 const PORT = 3000;
-const GMAIL_CLIENT_ID = "579714708688-f9t4vp17ea77hm42rfmk0p69s3s70r24.apps.googleusercontent.com";
-const GMAIL_CLIENT_SECRET = "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";
+const GMAIL_CLIENT_ID = "92499555227-bgav9kouetj259h41ghpofehno5gu272.apps.googleusercontent.com";
+const GMAIL_CLIENT_SECRET = "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";
 const GMAIL_REDIRECT_URL = "https://743d-2405-201-a007-cef6-14df-6efc-8822-135e.ngrok-free.app/auth/callback";
 
 const app = express();
+const pubSubClient = new PubSub();
 const oauth2Client = new OAuth2Client(GMAIL_CLIENT_ID, GMAIL_CLIENT_SECRET, GMAIL_REDIRECT_URL);
 
 // Body parser middleware
 app.use(express.json());
+
+async function createNotificationChannel() {
+    const gmail = google.gmail({ version: 'v1', auth: oauth2Client });
+    const res = await gmail.users.watch({
+        userId: 'me',
+        requestBody: {
+            topicName: 'projects/numeric-trilogy-416016/topics/real-estate-project',
+            labelIds: ['INBOX']
+        }
+    });
+    console.log(res.data);
+}
 
 app.get("/", (req, res) => {
     res.send("Hi you are in!");
@@ -22,7 +36,7 @@ app.get("/", (req, res) => {
 
 app.get('/auth', (req, res) => {
     const authUrl = oauth2Client.generateAuthUrl({
-        access_type: 'offline',
+        access_type: 'online',
         scope: ['https://www.googleapis.com/auth/gmail.readonly'],
     });
     res.redirect(authUrl);
@@ -33,6 +47,8 @@ app.get('/auth/callback', async (req, res) => {
     try {
         const { tokens } = await oauth2Client.getToken(code);
         oauth2Client.setCredentials(tokens);
+
+        await createNotificationChannel();
 
         const gmail = google.gmail({ version: 'v1', auth: oauth2Client });
         const messages = await gmail.users.messages.list({
@@ -70,10 +86,20 @@ app.get('/auth/callback', async (req, res) => {
     }
 });
 
-app.post('/notifications', async (req, res) => {
-    // Handle push notification payload here
-    console.log('Received push notification:', req.body);
-    res.status(200).end();
+app.post("/notifications", async (req, res) => {
+    try {
+        // Parse the incoming notification
+        const notification = req.body;
+
+        // Process the notification (e.g., log it)
+        console.log("Received notification:", notification);
+
+        // Acknowledge the notification to Pub/Sub
+        res.status(204).send();
+    } catch (error) {
+        console.error("Error handling notification:", error);
+        res.status(500).send("Internal Server Error");
+    }
 });
 
 app.listen(PORT, () => {
